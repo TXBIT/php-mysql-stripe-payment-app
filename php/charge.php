110db6bb47ed496e27aaff72b45c36638812e389
TXBIT <shenlong203@gmail.com>
2020-07-25 18:55:36 +0700

    Add Customer and Transaction

--- php/charge.php
+++ php/charge.php
@@ -1,5 +1,9 @@
 <?php
 require_once './vendor/autoload.php';
+require_once './config/db.php';
+require_once './lib/pdo_db.php';
+require_once './models/Customer.php';
+require_once './models/Transaction.php';
 
 \Stripe\Stripe::setApiKey('sk_test_26HMXpjJhgGj3EPS9BzuFkZU00garNsDVs');
 
@@ -24,6 +28,36 @@ $charge = \Stripe\Charge::create(array(
   "description" => "Intro to React Course",
   "customer" => $customer->id
 ));
+
+// Customer Data
+$customerData = [
+  'id' => $charge->customer,
+  'first_name' => $first_name,
+  'last_name' => $last_name,
+  'email' => $email
+];
+
+// Instantiate Customer
+$customer = new Customer();
+
+// Add Customer to DB
+$customer->addCustomer($customerData);
+
+// Transaction Data
+$transactionData = [
+  'id' => $charge->id,
+  'customer_id' => $charge->customer,
+  'product' => $charge->description,
+  'amount' => $charge->amount,
+  'currency' => $charge->currency,
+  'status' => $charge->status,
+];
+
+// Instantiate Transaction
+$transaction = new Transaction();
+
+// Add Transaction to DB
+$transaction->addTransaction($transactionData);
 
 // Redirect to success
 header('Location: success.php?tid=' . $charge->id . '&product=' . $charge->description);
